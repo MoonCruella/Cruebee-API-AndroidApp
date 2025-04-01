3545e06ff9b663fc6f62615ecb1e3eea499ec1c2
MunCruella <nhunguyetpy206@gmail.com>
2025-04-02 00:22:32 +0700

    Thêm chức api cho cart

--- src/main/java/androidapp/service/CartItemsService.java
+++ src/main/java/androidapp/service/CartItemsService.java
@@ -1,10 +1,12 @@
 package androidapp.service;
 
 import androidapp.entity.CartItemsEntity;
+import androidapp.model.CartItemsModel;
 
 import java.util.List;
 
 public interface CartItemsService {
-    public void addToCart(int userId, int productId, int quantity);
+    public void addToCart(CartItemsModel cartItemsModel);
     public List<CartItemsEntity> getUserCart(int userId);
+    void deleteToCart(CartItemsModel cartItem);
 }
